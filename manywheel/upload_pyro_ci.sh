0dc2446dd4aac29014578be7eb9479d1b2df494e
Neeraj Pradhan <npradhan@uber.com>
2018-01-25 11:21:36 -0800

    Pyro scripts for uploading PyTorch wheels to S3

---
+++ manywheel/upload_pyro_ci.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p pytorch-whls-bkp
+aws s3 sync s3://pyro-ppl/ci pytorch-whls-bkp --profile pyro-ci
+aws s3 rm s3://pyro-ppl/ci --recursive --profile pyro-ci
+ls -t wheelhousecpu/ | grep -m 2 -E "cp35m|cp27mu" | xargs -I {} bash -c 'aws s3 cp wheelhousecpu/{} s3://pyro-ppl/ci/ --acl public-read --profile pyro-ci'
